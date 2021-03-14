10176bfcf1935ccbc1c4221af4f28e70aeb34b9f
cdturner618 <cdturner618@gmail.com>
2021-03-14 12:10:14 -0500

    completed number 8

--- EmployeeSQL/homework.sql
+++ EmployeeSQL/homework.sql
@@ -47,4 +47,14 @@
 -- d.dept_no = departments.dept_no
 -- where departments.dept_name= 'Sales' or departments.dept_name= 'Development'
 
---8) In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--8) In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+-- select * from employees
+-- select last_name, count(last_name) as "Count of Last Names" from employees
+-- group by last_name
+
+
+
+
+
+
+
